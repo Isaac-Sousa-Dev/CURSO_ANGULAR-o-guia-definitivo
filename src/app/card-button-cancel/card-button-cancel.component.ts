3506f2a0555aca833c661e9e518495f50ba1f657
Isaac Sousa <114630360+Isaac-Sousa-Dev@users.noreply.github.com>
2025-01-06 21:19:06 -0300

    fix: referenciar arquivos de estilos

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,20 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styles: [
-    `
-      .card-cancel-button {
-          font-size: 16px;
-          color: white;
-          text-align: center;
-          margin-top: 10px;
-          padding: 10px;
-          font-weight: bold;
-          border-radius: 25px;
-          background-color: blue;
-      }
-    `
-  ]
+  styleUrls: ['./card-button-cancel.component.css'],
 })
 export class CardButtonCancelComponent {
 
