e37540e90f9d923bba8f8238b62b5f6cf8eca9d2
Isaac Sousa <114630360+Isaac-Sousa-Dev@users.noreply.github.com>
2025-01-10 23:18:32 -0300

    feat: view encapsulation shadow dom

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,7 +11,8 @@ interface IInfo {
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
-  encapsulation: ViewEncapsulation.Emulated
+  encapsulation: ViewEncapsulation.ShadowDom // Nada global afetará o componente, e nada do componente afetará o global
+  // encapsulation: ViewEncapsulation.Emulated // PADRÃO utilizado pelo Angular
   // encapsulation: ViewEncapsulation.None // Basicamente, remove o encapsulamento desse componente
 })
 export class CardComponent {
